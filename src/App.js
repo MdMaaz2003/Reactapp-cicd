dc621629d252d740bc656c7cd39bd5e102963513
MdMaaz2003 <maazmohd151@gmail.com>
2026-01-21 18:06:25 +0530

    Added react

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 React App Deployed via Jenkins + Docker</h1>
+      <p>Version: v1</p>
+    </div>
+  );
+}
+
+export default App;
